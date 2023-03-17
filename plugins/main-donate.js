8572de59e165dcb626fdaa4bdd50c95effc31ecd
Loother001 <122788306+Loother001@users.noreply.github.com>
2023-03-17 11:32:53 +0500

    Update main-donate.js

--- plugins/main-donate.js
+++ plugins/main-donate.js
@@ -6,7 +6,7 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 ONLY WANT YOUR LOVE
 SUPPORT ME BY MAKING 69 MILLION FOLLOWERS ON MY INSTAGRAM`
 let img = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg'
-conn.sendHydrated2(m.chat, don, '▢ MAY LORD JAGANNATH BLESS YOUR DAY\n▢ ᴳᵁᴿᵁ  ┃ ᴮᴼᵀ\n▢ Subscribe  Youtube\nhttps://youtube.com/@Asliguru\n', img,'https://www.instagram.com/asli_guru69', 'Instagram', 'https://t.me/i_want_to_be_isekaied', 'TELEGRAM', [
+conn.sendHydrated2(m.chat, don, '▢ MAY LORD  BLESS YOUR DAY\n923466056497\n▢ Subscribe  Youtube\n923466056497\n', img,'923466056497', 'Instagram', '923466056497', 'TELEGRAM', [
     ], m)
 }
 
